2af962215a1799ab63e483bc04f52e0459a0bf1e
Adios <30585897+jongwanS@users.noreply.github.com>
2023-06-03 17:18:29 +0900

    feat:인프라레이어 개발 추가

---
+++ reservation-study/src/main/java/com/jwcinema/ticketing/domain/DiscountCalculationService.java
@@ -0,0 +1,14 @@
+package com.jwcinema.ticketing.domain;
+
+import com.jwcinema.discount.domain.OrderDiscount;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DiscountCalculationService {
+    public double calculateDiscountedPrice(long ticketPrice, Optional<OrderDiscount> orderDiscount) {
+        return orderDiscount.map(discount -> discount.calculateDiscountPrice(ticketPrice)).orElse(0L);
+    }
+}
